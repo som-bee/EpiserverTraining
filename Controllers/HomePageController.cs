52882f91140d31ed1f2072ab7176d84530187158
Somnath Bera <somnathbera1919@gmail.com>
2023-07-20 13:07:22 +0530

    Episerver Find & Search configured and tested in the Home Controller

--- Controllers/HomePageController.cs
+++ Controllers/HomePageController.cs
@@ -10,6 +10,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
+using EPiServer.Find;
+using EPiServer.Find.Cms;
+using EPiServer.Filters;
 
 namespace EpiserverTraining.Controllers
 {
@@ -17,17 +20,21 @@ namespace EpiserverTraining.Controllers
     {
         private Injected<IPageBuilder>  _pageBuilder;
 
+        private readonly IClient _searchClient;
 
 
-        //public HomePageController(IPageBuilder pageBuilder)
-        //{
-        //    _pageBuilder = pageBuilder;
-        //}
+
+        public HomePageController(IClient searchClient)
+        {
+            _searchClient = searchClient;
+        }
 
         public ActionResult Index(HomePage currentPage)
         {
 
             //performCRUD();
+
+            EpiFindTest();
 
 
             var viewModel = new HomePageViewModel(currentPage)
@@ -37,6 +44,53 @@ namespace EpiserverTraining.Controllers
             
 
             return View(viewModel);
+        }
+
+        private void EpiFindTest()
+        {
+
+            //###################### FIND
+            var test1 = _searchClient.Search<ArticlePage>()
+                //.Filter(x => x.Name.MatchCaseInsensitive("Tech Blogs"))
+                .GetContentResult();
+
+            //#########################
+
+            	var test = _searchClient.UnifiedSearchFor("Blog").GetResult();
+
+            // 2000 of articlePage which as name equal to start, 1K
+            //var test1 = _searchClient.Search<ArticlePage>()
+            //	.For("Alloy")
+            //   // .Filter(x => x.Name.MatchCaseInsensitive("Alloy Saves Bears"))
+            //   	.TermsFacetFor(x => x.Name)
+            //	.GetContentResult();
+
+
+            //###################### SEARCH
+            var criterias = new PropertyCriteriaCollection
+             {
+             	//new PropertyCriteria()
+             	//{
+             	//	Name = "PageName",
+             	//	Type = PropertyDataType.String,
+             	//	Condition = EPiServer.Filters.CompareCondition.StartsWith,
+             	//	Value = "Alloy"
+             	//},
+             	new PropertyCriteria()
+                 {
+                     Condition = CompareCondition.Equal,
+                     Name = "PageTypeName",
+                     Type = PropertyDataType.PageType,
+                     Value = "ArticlePage",
+                     Required = true
+                 }
+             };
+
+            var repository = ServiceLocator.Current.GetInstance<IPageCriteriaQueryService>();
+            var pages = repository.FindPagesWithCriteria(
+                 PageReference.StartPage,
+                 criterias);
+
         }
 
         private void performCRUD()
